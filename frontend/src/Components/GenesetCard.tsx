9d3255472df11ddbd467f3d0100d0e83e7316cf4
Lucie Hélias <lucie.helias@protonmail.com>
2024-11-18 15:11:55 +0100

    Create GenesetCard component to display individual geneset

---
+++ frontend/src/Components/GenesetCard.tsx
@@ -0,0 +1,39 @@
+type Gene = {
+  id: number;
+  name: string;
+};
+
+type Geneset = {
+  id: number;
+  title: string;
+  genes: Gene[];
+};
+
+type GenesetCardProps = {
+  geneset: Geneset;
+};
+
+const GenesetCard = ({ geneset }: GenesetCardProps) => {
+  return (
+    <li
+      key={geneset.id}
+      className="bg-violet-700 p-4 rounded-lg shadow-md hover:shadow-lg transition duration-300"
+    >
+      <h2 className="text-3xl text-center font-semibold text-white mb-6">
+        {geneset.title}
+      </h2>
+      <ul className="grid grid-cols-2 gap-4">
+        {geneset.genes.map((gene, index) => (
+          <li
+            key={index}
+            className="text-lg text-center rounded-lg bg-white text-teal-500 p-2 "
+          >
+            {gene.name}
+          </li>
+        ))}
+      </ul>
+    </li>
+  );
+};
+
+export default GenesetCard;
